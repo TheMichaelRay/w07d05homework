b24d913e9f576dc44151aacc074305efa19a9335
Shahrooz <shahrooz84@gmail.com>
2016-06-17 15:52:43 -0700

    User routes finished

--- routes/users.js
+++ routes/users.js
@@ -11,3 +11,39 @@ app.get('/users', function(req, res){
     res.json(result);
   })
 });
+
+app.get('/users/:id', function(req, res){
+  User.findById(req.params.id, function(err, user){
+    if(err) return console.log(err)
+    res.json(user)
+  })
+})
+
+app.patch('/users/:id', function(req, res){
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true} , function(err, question) {
+    if (err) console.log(err)
+    res.json({message: "User updated!", success: true, user: user})
+  })
+})
+
+app.delete('/users/:id', function(req, res){
+  User.findByIdAndRemove(req.params.id, function(err, question) {
+    if (err) console.log(err)
+    res.json({message: "User removed!", success: true, user: user})
+  })
+})
+
+app.get('/createUser',function(req,res){
+  console.log(req.query);
+  var name="";
+  var age ="";
+  var email ="";
+  if(req.query.name){
+    name = req.body.name;
+  }
+  if(req.query.age){
+    age = req.body.age;
+  }
+  if(req.query.email){
+    email = req.body.email;
+  }
