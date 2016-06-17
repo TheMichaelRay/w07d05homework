5437e3653924a2e1b8e89fcb206b545751a1394b
lawrencegomez <lfg2305@gmail.com>
2016-06-17 15:41:46 -0700

    created the User model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,21 @@ var
   mongoose = require('mongoose'),
   Schema = mongoose.Schema,
   userSchema = new Schema({
-    name: {type: String, required: true}
-    age: 'number',
-    email: 'string'
+    name: {type: String, required: true},
+    age: {type: Number, required: true},
+    email: {type: String, required: true, unique: true},
+    created_at: Date,
+    updated_at: Date
   })
+
+userSchema.pre('save', function(next){
+  var current_date = new Date
+  this.updated_at = current_date
+  if (!this.created_at) {
+    this.created_at = current_date
+  }
+  next()
+})
+
+var User = mongoose.model('User', userSchema)
+module.exports = User
