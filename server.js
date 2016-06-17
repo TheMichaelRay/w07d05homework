148d65ad8f77227d8318e3af2219ce2acd8c47f7
Michael Ray <raymichael23@gmail.com>
2016-06-17 15:22:01 -0700

    added module variables and server listeners

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-  
+var
+  express = require('express'),
+  mongoose = require('mongoose'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  PORT = 3000;
+
+
+
+
+app.listen(PORT, function(err){
+  if (err) throw err;
+  console.log("Server running on port:", PORT)
+})
